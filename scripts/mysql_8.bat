71a6002ae307093e8579ebc9181cc15cd0b32b3f
artistotless <a.boziev@logareon.ru>
2024-02-11 09:47:35 +0300

    put dlls into identity project

--- scripts/mysql_8.bat
+++
@@ -1,3 +0,0 @@
-docker run -d -p 3506:3306 -v C:\BusyBox\mysql_8_volume:/var/lib/mysql -v C:\BusyBox\mysql_8_init:/etc/mysql/conf.d -v C:\BusyBox\mysql_8_init:/docker-entrypoint-initdb.d -e "MYSQL_USER=user" -e "MYSQL_PASSWORD=userPass" -e "MYSQL_ROOT_PASSWORD=rootPass" --name mysql_dev_8_0 mysql:8.0
-
-pause
--- scripts/rabbitMQ.bat
+++
@@ -1,3 +0,0 @@
-docker run -d -p 5672:5672 -p 15673:15672 -v C:\BusyBox\rabbit_volume:/var/lib/rabbitmq -e "RABBITMQ_DEFAULT_PASS=@admin@" -e "RABBITMQ_DEFAULT_USER=admin" --name rabbit_dev rabbitmq:3-management
-
-pause
