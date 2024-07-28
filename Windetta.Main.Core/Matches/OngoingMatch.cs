ead1cea570b8aaf156d4475ce4630552b079c6b6
artistotless <neuralink7232050@gmail.com>
2024-07-28 16:01:52 +0300

    Task#OmVEXTA3 refactored IOngoingMatches, replaced nugets by csproject references

--- Windetta.Main.Core/Matches/OngoingMatch.cs
+++ Windetta.Main.Core/Matches/OngoingMatch.cs
@@ -33,8 +33,8 @@ public sealed class OngoingMatch
     /// </summary>
     public DateTimeOffset Created { get; init; }
 
-    /// <summary>
-    /// GameServer endpoint
-    /// </summary>
-    public Uri GameServerEndpoint { get; set; }
+    public OngoingMatch(Guid id, Guid gameId, IEnumerable<Player> players)
+    {
+        Created = DateTimeOffset.UtcNow;
+    }
 }
