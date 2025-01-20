129d8ba18f8267015cc93d61db8468710365ed9c
artistotless <neuralink7232050@gmail.com>
2025-01-20 15:43:47 +0300

    fixed bad links

--- misc/windetta-services_run_fake_auth.bat
+++ misc/windetta-services_run_fake_auth.bat
@@ -7,7 +7,7 @@ set ASK_DEBUGGING_ATTACH=No
 cd E:\Projects\windetta\Windetta.Main\src\bin\Debug\net8.0\
 start /min "Main:55001" dotnet Windetta.Main.dll
 
-cd E:\Projects\lspm\src\bin\Debug\net8.0\
+cd E:\Projects\windetta\lspm\lspm\src\bin\Debug\net8.0\
 start /min "LSPM:55005" dotnet LSPM.dll
 
 cd E:\Projects\windetta\Windetta.Wallet\src\bin\Debug\net8.0\
@@ -16,7 +16,7 @@ start /min "Wallet:55003" dotnet Windetta.Wallet.dll
 cd E:\Projects\windetta\Windetta.Identity\src\bin\Debug\net8.0\
 start /min "Identity:55002" dotnet Identity.dll
 
-cd E:\Projects\windetta-mvc\src\bin\Debug\net9.0\
+cd E:\Projects\windetta\Windetta.Web\src\bin\Debug\net9.0\
 start /min "Web:55004" dotnet Windetta.Web.dll
 
 exit
